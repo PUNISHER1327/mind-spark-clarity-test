838633c38ecdd4eeaab97719a80a16fe06860f99
Aditya Manhas <134808877+PUNISHER1327@users.noreply.github.com>
2025-05-24 12:13:45 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,8 +53,8 @@ export const Navbar = () => {
       >
         <div className="container flex items-center justify-between">
           <Link to="/" className="flex items-center">
-            <span className="text-2xl font-bold text-primary">Dyslexia</span>
-            <span className="text-2xl font-medium ml-1">Test</span>
+            <span className="text-2xl font-bold text-primary">Dys</span>
+            <span className="text-2xl font-medium ml-1">Cover</span>
           </Link>
 
           {/* Desktop Navigation */}
